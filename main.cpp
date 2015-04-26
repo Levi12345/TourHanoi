096e71e39a34bc217ba495fc38bd962dd1a1b72b
Levi12345 <levi.mischael.assi@gmail.com>
2015-04-26 17:05:01 +0000

    bhvbtg

--- main.cpp
+++ main.cpp
@@ -5,18 +5,10 @@ int main ( int argc, char** argv )
 {
     SDL_Surface *ecran = NULL;
     SDL_Surface *imageprincipale = IMG_Load("images/hanoii0.jpg");
-
     SDL_Surface *jouer_premier = IMG_Load("images/reset.png");
-    //SDL_SetColorKey(jouer_premier,SDL_SRCCOLORKEY,SDL_MapRGB(jouer_premier->format,255,255,255));
-
     SDL_Surface *instructions_premier = IMG_Load("images/instruction.png");
-    SDL_SetColorKey(instructions_premier,SDL_SRCCOLORKEY,SDL_MapRGB(instructions_premier->format,255,255,255));
-
     SDL_Surface *scores_premier = IMG_Load("images/mscore.png");
-    SDL_SetColorKey(scores_premier,SDL_SRCCOLORKEY,SDL_MapRGB(scores_premier->format,255,255,255));
-
     SDL_Surface *apropos_premier = IMG_Load("images/apropos.png");
-    //SDL_SetColorKey(apropos_premier,SDL_SRCCOLORKEY,SDL_MapRGB(apropos_premier->format,255,255,255));
 
     SDL_Rect positionimage , positionbouton;
 
@@ -30,16 +22,16 @@ int main ( int argc, char** argv )
 
     positionbouton.x=2.5;
     positionbouton.y=400;
-    SDL_BlitSurface(jouer_premier , NULL , ecran ,&positionbouton );
+    SDL_BlitSurface(jouer_premier , NULL , ecran ,&positionbouton);
 
     positionbouton.x=207.5;
-    SDL_BlitSurface(instructions_premier , NULL , ecran ,&positionbouton );
+    SDL_BlitSurface(instructions_premier , NULL , ecran ,&positionbouton);
 
     positionbouton.x=412.5;
-    SDL_BlitSurface(scores_premier , NULL , ecran ,&positionbouton );
+    SDL_BlitSurface(scores_premier , NULL , ecran ,&positionbouton);
 
     positionbouton.x=617.5;
-    SDL_BlitSurface(apropos_premier , NULL , ecran ,&positionbouton );
+    SDL_BlitSurface(apropos_premier , NULL , ecran ,&positionbouton);
 
     SDL_Event event;
     int continuer=1;
@@ -51,15 +43,41 @@ int main ( int argc, char** argv )
         case SDL_QUIT:
             continuer=0;
             break;
+        case SDL_MOUSEBUTTONDOWN:
+            switch(event.button.button)
+            {
+            case SDL_BUTTON_LEFT:
+                if(event.button.x>=2.5 && event.button.x<=182.5 && event.button.y>=400 && event.button.y<=440)
+                {
+                    SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,255,0,0));
+                }
+                if(event.button.x>=207.5 && event.button.x<=387.5 && event.button.y>=400 && event.button.y<=440)
+                {
+                    SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,0,255,0));
+                }
+                if(event.button.x>=207.5 && event.button.x<=387.5 && event.button.y>=400 && event.button.y<=440)
+                {
+                    SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,0,255,0));
+                }
+                if(event.button.x>=412.5 && event.button.x<=592.5 && event.button.y>=400 && event.button.y<=440)
+                {
+                    SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,0,0,255));
+                }
+                if(event.button.x>=617.5 && event.button.x<=797.5 && event.button.y>=400 && event.button.y<=440)
+                {
+                    SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,0,255,0));
+                }
+            }
+
         }
         SDL_Flip(ecran);
     }
-
-
-
-
-
-
      SDL_Quit();
     return 0;
+}
+
+
+int page1(SDL_Surface* ecran)
+{
+   SDL_Surface *imageprincipale = IMG_Load("images/hanoii0.jpg");
 }
